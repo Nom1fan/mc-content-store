1c9c3d22870e48fea6cb09f2072d82821d3c645b
Mor <mormerhav@gmail.com>
2016-09-02 09:44:11 +0300

    Forgot this

---
+++ sample/src/main/java/com/nostra13/universalimageloader/sample/fragment/CustomVideoView.java
@@ -0,0 +1,25 @@
+package com.nostra13.universalimageloader.sample.fragment;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * Created by A527307 on 12/08/2016.
+ */
+public class CustomVideoView extends VideoView {
+
+
+    public CustomVideoView(Context context) {
+        super(context);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
